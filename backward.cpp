684221b7b2d11d92bd6269874b0d060cec2f2652
Chuanlu Liu <57776207+syndoom@users.noreply.github.com>
2022-04-25 19:58:34 -0400

    Update backward.cpp

--- backward.cpp
+++ backward.cpp
@@ -17,21 +17,21 @@
 
 // - apt-get install libdw-dev ...
 // - g++/clang++ -ldw ...
-// #define BACKWARD_HAS_DW 1
+#define BACKWARD_HAS_DW 1
 
 // - apt-get install binutils-dev ...
 // - g++/clang++ -lbfd ...
-// #define BACKWARD_HAS_BFD 1
+#define BACKWARD_HAS_BFD 1
 
 // - apt-get install libdwarf-dev ...
 // - g++/clang++ -ldwarf ...
-// #define BACKWARD_HAS_DWARF 1
+#define BACKWARD_HAS_DWARF 1
 
 // Regardless of the library you choose to read the debug information,
 // for potentially more detailed stack traces you can use libunwind
 // - apt-get install libunwind-dev
 // - g++/clang++ -lunwind
-// #define BACKWARD_HAS_LIBUNWIND 1
+#define BACKWARD_HAS_LIBUNWIND 1
 
 #include "backward.hpp"
 
